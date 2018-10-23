99801f38519dd1bf4bd8845387396b6fb81b20c6
Devrajsinghania <devrajsinghania79@gmail.com>
2018-10-23 13:35:08 +0530

    Marks
    printing average with subjects

---
+++ Marks2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+	
+	  public static void main(String[] args) {
+
+        char a;
+        int x, y, z;
+        Scanner store = new Scanner(System.in);
+        a = store.next().charAt(0);
+        x=store.nextInt();
+        y=store.nextInt();
+        z=store.nextInt();
+
+       
+        }
+    }
+}
