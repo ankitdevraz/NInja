6391040f06f1ccf9ca77facd8d41c6b3cc18e3d2
ankitdevraz <44330427+ankitdevraz@users.noreply.github.com>
2018-10-23 13:30:52 +0530

    Loop is missing
    for better print

--- Pattern.java
+++ Pattern.java
@@ -7,7 +7,16 @@ public class Solution {
 		int n = sc.nextInt();
 		
 //print the loop
-
+ for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+        for(int k=1; k<=i; k++){
+          System.out.print("*");
+        }
+        for(int l=2; l<=i; l++){
+          System.out.print("*");
+        }
      
 
         System.out.println();
