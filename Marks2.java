6d0e7fa4674cdbcdd795f4342106798755529584
ankitdevraz <44330427+ankitdevraz@users.noreply.github.com>
2018-10-23 13:36:41 +0530

    Complete the code
    Code is now completed

--- Marks2.java
+++ Marks2.java
@@ -11,6 +11,16 @@ public class Main {
         y=store.nextInt();
         z=store.nextInt();
 
+        if(x>=y && y>=z) System.out.println(a+" "+(x+y)/2);
+
+        else if (x>=y && z>=y) System.out.println(a+" "+(x+z)/2);
+        else if (y>=x && x>=z) System.out.println(a+" "+(y+z)/2);
+        else if (y>=x && z>=x) System.out.println(a+" "+(y+z)/2);
+        else if (z>=x && x>=y) System.out.println(a+" "+(x+z)/2);
+
+        else {
+            System.out.println(a+" "+(z+y)/2);
+        }
        
         }
     }
