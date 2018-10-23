0c39676b676db7be574911a1c9a499a2fa445d32
Devrajsinghania <devrajsinghania79@gmail.com>
2018-10-23 13:39:49 +0530

    fianl
    finalize the code

---
+++ final5.java
@@ -0,0 +1,22 @@
+
+public class Solution {
+
+	public static int[] allIndexes(int input[], int x) {
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+	 	 * Taking input and printing output is handled automatically.
+		*/
+		return arrayIndex(input,x,0);
+	}
+   public static int[] arrayIndex (int [] arr, int ele, int startIndx){
+       if(startIndx==arr.length){
+           int [] rr= {};
+           return rr;
+       }
+
+       int[]  returnofIndexes = arrayIndex(arr,ele,startIndx+1);
+           if(arr[startIndx]==ele){
+               int [] totalIndex= new int[returnofIndexes.length+1];
+               totalIndex[0]=startIndx;
