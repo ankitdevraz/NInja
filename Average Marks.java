3986dc5d0ed2a4e42ac5e93a84ca0cbb0f499f4e
ankitdevraz <44330427+ankitdevraz@users.noreply.github.com>
2018-10-23 13:18:52 +0530

    Completing the code
    After completeing the code it will work fine

--- Average Marks.java
+++ Average Marks.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner // Scanner is missing in the given Code
 
 
 public class Solution {
@@ -10,11 +10,14 @@ public class Solution {
       
       String name=sc.nextLine();
       int	 m1=sc.nextInt();
+	int m2=sc.nextInt(); //input of marks 2	
+      int m3=sc.nextInt(); //taking the input of marks 3
+		
+      int avg=(m1+m2+m3)/3; // Finding the average marks
       
-      int avg=(m1+m2+m3)/3;
-      
-      System.out.println(name);
-      System.out.println(avg);
+      System.out.println(name); // Printing the name of subject		
+		
+      System.out.println(avg);   // printing the average marks.
 		
 	
 		
